a41721486da4f9567359215460f5cd3aa24d98f1
Henri Manson <h.manson@aal-audio.com>
2023-03-09 10:00:40 +0100

    Android studio + openjdk-kerberos

---
+++ app/src/main/java/com/google/android/apps/work/kerberosauthenticator/Strings.java
@@ -0,0 +1,7 @@
+package com.google.android.apps.work.kerberosauthenticator;
+
+public class Strings {
+    public static boolean isNullOrEmpty(String string) {
+        return string == null ? true : string.isEmpty();
+    }
+}
